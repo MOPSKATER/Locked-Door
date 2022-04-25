4917e52cdd483ea4d698511837c614fd9fb34fd2
oberkater <josua.w@gmx.de>
2022-04-25 14:57:39 +0200

    prepared for gunicorn

--- main.py
+++ main.py
@@ -4,25 +4,27 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 
-if not len(argv) == 2:
-    print("usage: python3 main.py <secret key>")
-    exit(1)
+def gunicorn(key):
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = argv[1]
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///access.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    if not key:
+        print("usage: python3 main.py <secret key>")
+        exit(1)
 
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = "login"
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = key
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///access.db"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = "login"
 
-import models
-import routes
-import utility
+    db = SQLAlchemy(app)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, ssl_context=(
-        'cert.pem', 'key.pem'), debug=True)
+    import models
+    import routes
+    import utility
+
+    if __name__ == "__main__":
+        app.run(host="0.0.0.0", port=8080, ssl_context=(
+            'cert.pem', 'key.pem'), debug=True)
