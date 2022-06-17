0f1931168aaf3a132e603e6602ad0bb3f57ca2d2
oberkater <josua.w@gmx.de>
2022-06-17 13:34:50 +0200

    Reenabled ssl

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,12 @@
 from main import app
 
-def run():
-    app.run()
-        
+
+def run(ssl=True):
+    if ssl:
+        app.run(ssl_context=('fullchain.pem', 'privkey.pem'))
+    else:
+        app.run()
+
+
 if __name__ == "__main__":
     run()
